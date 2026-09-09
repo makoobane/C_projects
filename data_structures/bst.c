596d10d9a66cf1ba44585f3f8ba0eabe3aa51c23
Asalomedia <mediaasalo@gmail.com>
2026-09-09 13:43:43 +0300

    bst is added finmax,finmin, find  , contain and remove

--- data_structures/bst.c
+++ data_structures/bst.c
@@ -12,12 +12,32 @@ typedef struct BST
 }BST;
 BST* createBSTWithParentDataOf(int data);
 void append(BST* tree,int data);
+int contains(BST* tree, int data);
+Node* find(BST* tree, int data);
+Node* findParentOf(BST* tree, int data);
+Node* findMax(BST* tree);
+Node* findMin(BST* tree);
+void remove(BST* tree, int data);
 void deallocate(BST* tree);
 void freeing(Node* root);
 int main(){
     BST* tree=createBSTWithParentDataOf(10);
     if(tree!=NULL){
       append(tree,12);
+      append(tree,6);
+      append(tree,2);
+      append(tree,20);
+      append(tree,13);
+      append(tree,101);
+      append(tree,61);
+      append(tree,-22);
+      append(tree,110);
+      append(tree,0);
+      append(tree,16);
+      printf("contains: %d\n",contains(tree,62));
+      Node* n=find(tree,-20);
+      if(n!=NULL) printf("found?%d\n",n->data);
+      printf("min in this tree is : %d, where max is : %d\n",findMin(tree)->data,findMax(tree)->data);
       deallocate(tree);
     }
 
@@ -88,6 +108,116 @@ puts("it appended successfully");
     perror("it failed to allocate that inserted node");
     exit(1);
 }
+}
+
+int contains(BST *tree, int data)
+{
+    Node* current=tree->parent;
+    while (current!=NULL)
+    {
+       if(current->data==data){
+          return 1;
+       }else if(data>current->data){
+        current=current->right;
+       }else{
+        //data<current.data
+        current=current->left;
+       }
+    }
+    return 0;
+    
+}
+
+Node *find(BST *tree, int data)
+{
+    Node* current=tree->parent;
+    while (current!=NULL)
+    {
+       if(current->data==data){
+          return current;
+       }else if(data>current->data){
+        current=current->right;
+       }else{
+        current=current->left;
+       }
+    }
+    printf("No such node with data of %d,you can register it now with \"append(tree,data)\" function\n",data);
+    return NULL;
+}
+
+Node *findParentOf(BST *tree, int data)
+{
+    Node* parent=NULL;
+    Node* current=tree->parent;
+    while (current!=NULL)
+    {
+        if(current->data==data){
+          return current;
+       }else if(data>current->data){
+        //keep this as parent before you go down
+        parent=current;
+        current=current->right;
+       }else{
+        parent=current;
+        current=current->left;
+       }
+    }
+    return parent;
+}
+
+Node *findMax(BST *tree)
+{  
+    //max is righ most node always available if not parent is max and min at same time hhh
+    Node* current=tree->parent;
+    while (current->right!=NULL)
+    {
+        current=current->right;
+    }
+    return current;
+}
+
+Node *findMin(BST *tree)
+{
+    //minimum is left most node
+    Node* current=tree->parent;
+    while(current->left!=NULL){
+        current=current->left;
+    }
+    return current;
+}
+
+void remove(BST *tree, int data)
+{
+    Node* node=find(tree,data);
+    if(node!=NULL){
+      Node* parent=findParentOf(tree,data);
+      if(parent==NULL){
+        // node is main parent then don't remove it 
+        printf("you cant remove main parent of the tree ... atleast for now\n");
+        return ;
+      }
+      //divide this to three cases that i learned from claude for now ... atleast
+      //state one: it has no children , free it and stop from its parents pointing it
+      if(node->left==NULL&&node->right==NULL){
+        if(parent->left==node) parent->left=NULL;
+        if(parent->right==node) parent->right=NULL;
+        free(node);
+      }
+      //state two: it has one child, move that child its position
+      if(node->left!=NULL||node->right!=NULL &&!(node->left!=NULL&&node->right!=NULL)){
+        Node* child=node->left==NULL?node->right:node->left;
+        if(parent->left==node) parent->left=child;
+        if(parent->right==node)parent->right=child;
+        free(node);
+      }
+      //state three: it has ttwo children,
+
+
+    }else{
+     //link this string below what msg that find gave us
+     printf("------:so you cant remove what you dont have"\n);
+     return;
+    }
 }
 
 void freeing(Node* root)
