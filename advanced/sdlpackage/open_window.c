92697c402bc83e630bceedf89021e3e5c04e2b18
Asalomedia <mediaasalo@gmail.com>
2026-09-13 12:26:38 +0300

    open window in SDL2

--- advanced/sdlpackage/open_window.c
+++ advanced/sdlpackage/open_window.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+struct Game{
+SDL_Window * window;
+SDL_Renderer * renderer;
+};
+bool initialize_SDL(struct Game* game);
 int main(){
     
     return 0;
+}
+bool initialize_SDL(struct Game* game){
+    int error=SDL_Init(SDL_INIT_EVERYTHING);
+    if(error==0){
+        return true;
+    }else{
+        fprintf(stderr,"error happened at initialization: %s\n",SDL_GetError);
+        return false;
+    }
 }
