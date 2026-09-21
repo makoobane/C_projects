9458169f2fddf555a9c95e5c85026f350d379479
Asalomedia <mediaasalo@gmail.com>
2026-09-21 08:49:04 +0300

    complete game of life

--- advanced/game_of_life/main.c
+++ advanced/game_of_life/main.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 
 #include <SDL2/SDL.h>
+#define TITLE "GAME OF LIFE"
 struct Box
 {
     bool isAlive;
@@ -15,21 +16,35 @@ struct Game{
     SDL_Window * window;
     SDL_Renderer* renderer;
     struct Box** boxes;
+    struct Box** alt_boxes;
     int how_many_boxes_in_side;
     int width;
     int height;
     int s_side_of_box;
     int padding;//padding between boxes and their outside
     enum GameState state;
+    int frame_counter;
+    int frames_per_step;
 };
+//SDL_related functions
 bool initializeGame(struct Game* game);
 void drawGridAndFillAliveOnes(struct Game* game);
+//game related functions
+void clickBox(int x, int y,struct Game* game);
+struct Box** allocateBoxes(int how_many);
+
+int  howManyLiveNeighbors(int row,int col,int how_many_each_dim,struct Box** boxes);
+void run(struct Game* game);
+void freeBoxes(struct Box** boxes,int how_many_in_side);
 void freeing(struct Game* game,int exit_status);
 int main(){
     struct Game game={
+        .frame_counter=0,
+        .frames_per_step=6,
+        .alt_boxes=NULL,
         .state=PUTTING,
         .padding=1,
-        .how_many_boxes_in_side=20,// try 20 first
+        .how_many_boxes_in_side=30,// try 20 first
         .s_side_of_box=0,
         .width=600,
         .height=600,
@@ -53,13 +68,15 @@ int main(){
                    break;
                 case SDL_MOUSEBUTTONDOWN:
                     if(game.state==PUTTING){
-                         int x=event.motion.x;
-                         int y=event.motion.y;
-                         int col=x/game.s_side_of_box;
-                         int row=y/game.s_side_of_box;
-                    
-                         game.boxes[row][col].isAlive=true;
+                         int x=event.button.x;
+                         int y=event.button.y;
+                         clickBox(x,y,&game);
                     }
+                    break;
+                case SDL_MOUSEMOTION:
+                    if (game.state == PUTTING && (event.motion.state & SDL_BUTTON_LMASK)) {
+                    clickBox(event.motion.x, event.motion.y,&game);
+                   }
                     break;
                 case SDL_KEYDOWN://listen key pressed
                 switch (event.key.keysym.scancode)
@@ -70,8 +87,12 @@ int main(){
                         break;
                     // if space is clicked then we want to change state of the game
                     case SDL_SCANCODE_SPACE:
-                        if(game.state==1) game.state=RUNNING;
-                        if(game.state==2) game.state=PUTTING;
+                        if(game.state==PUTTING) {
+                            game.state=RUNNING;
+                        }
+                        else {
+                            game.state=PUTTING;
+                        }
                         break;
                     default:
                         break;
@@ -81,6 +102,7 @@ int main(){
                     break;
             }
         }
+        run(&game);
         //render screen
         SDL_SetRenderDrawColor(game.renderer,0,0,0,255);//black background
         //clear frame
@@ -99,7 +121,7 @@ int main(){
 bool initializeGame(struct Game *game)
 {
    //init sdl
-   int init_error=  SDL_Init(SDL_INIT_EVERYTHING);
+   int init_error=  SDL_Init(SDL_INIT_VIDEO);
    if(init_error!=0){
       fprintf(stderr,"error on sdl_init:%s\n",SDL_GetError());
       return false;
@@ -117,7 +139,7 @@ bool initializeGame(struct Game *game)
    game->width=displayMode.h;
    game->height=displayMode.h;
    //create window
-   SDL_Window* window=SDL_CreateWindow("GAME OF LIFE",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,game->width,game->height,0);
+   SDL_Window* window=SDL_CreateWindow(TITLE,SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,game->width,game->height,0);
    if(window==NULL){
       fprintf(stderr,"error at window_creation: %s\n",SDL_GetError());
       return false;
@@ -134,21 +156,24 @@ bool initializeGame(struct Game *game)
    game->s_side_of_box=game->width/game->how_many_boxes_in_side;
    printf("size of box: %d\n",game->s_side_of_box);
    // allocate boxes 
-   game->boxes=(struct Box**)malloc(game->how_many_boxes_in_side* sizeof(struct Box*));
-   for(int i=0;i<game->how_many_boxes_in_side;i++){
-      game->boxes[i]=(struct Box*)malloc(game->how_many_boxes_in_side*sizeof(struct Box));
-   }
+  game->boxes=allocateBoxes(game->how_many_boxes_in_side);
    if(game->boxes==NULL){
       printf("error happened at allocating %d X %d boxes\n",game->how_many_boxes_in_side,game->how_many_boxes_in_side);
       return false;
    }
-   //fill the boxes that they are not alive
+   //alocate temperory boxes;
+   game->alt_boxes=allocateBoxes(game->how_many_boxes_in_side);
+   if(game->alt_boxes==NULL){
+      printf("error happened at allocating %d X %d alt_boxes\n",game->how_many_boxes_in_side,game->how_many_boxes_in_side);
+      return false;
+   }
+   //fill the boxes and temp(optional) that they are not alive
    for(int row=0;row<game->how_many_boxes_in_side;row++){
       for(int col=0;col<game->how_many_boxes_in_side;col++){
          game->boxes[row][col].isAlive=false;
+         game->alt_boxes[row][col].isAlive=false;
       }
    }
-//    game->boxes[10][10].isAlive=true;//to view alive fill color
    
 
    return true;
@@ -177,14 +202,106 @@ void drawGridAndFillAliveOnes(struct Game *game)
     }
 }
 
+
+int howManyLiveNeighbors(int row, int col, int how_many_each_dim,struct Box **boxes)
+{
+    int how_many_lives=0;
+    //farthest column
+    int starting_col=col-1;
+    int ending_col=col+1;
+    //farthest row
+    int starting_row=row-1;
+    int ending_row=row+1;
+    //adjust columns if they are first or last 
+    if(starting_col==-1) starting_col=col;
+    if(ending_col==how_many_each_dim) ending_col=col;
+    //adjust row just similar to columns
+    if(starting_row==-1) starting_row=row;
+    if(ending_row ==how_many_each_dim) ending_row=row;
+    //count 
+    for(int r=starting_row;r<=ending_row;r++){
+        for(int cl=starting_col;cl<=ending_col;cl++){
+           if(r==row && cl==col){
+            //just skip it is the main cell
+            continue;
+           }else{
+            //this time we are at neighbors
+            if(boxes[r][cl].isAlive){
+                //if neighbor is alive count as how many lives
+                how_many_lives+=1;
+            }
+           }
+        }
+    }
+    return how_many_lives;
+}
+
+void run(struct Game *game)
+{
+    if(game->state==RUNNING){
+        //each frames per step it runs once others it skipps
+            game->frame_counter++;
+            if (game->frame_counter < game->frames_per_step) return;
+            game->frame_counter = 0;
+            for(int row=0;row<game->how_many_boxes_in_side;row++){
+                for(int col=0;col<game->how_many_boxes_in_side;col++){
+                    int aliveNeighbors = howManyLiveNeighbors(row, col,
+                        game->how_many_boxes_in_side, game->boxes);  
+                    if (game->boxes[row][col].isAlive) {                                 
+                       game->alt_boxes[row][col].isAlive = (aliveNeighbors == 2 || aliveNeighbors == 3);
+                    } else {
+                       game->alt_boxes[row][col].isAlive = (aliveNeighbors == 3);
+                    }
+                }
+            }
+            //swaap boxes
+            struct Box** tmp = game->boxes;
+            game->boxes = game->alt_boxes;
+            game->alt_boxes = tmp;
+          
+    }
+}
+
+void freeBoxes(struct Box **boxes, int how_many_in_box_side)
+{
+    if(boxes==NULL) return;
+    for(int i=0;i<how_many_in_box_side;i++){
+            free(boxes[i]);
+    }
+    free(boxes);
+}
+
+void clickBox(int x, int y,struct Game* game)
+{
+    int col=x/game->s_side_of_box;
+    int row=y/game->s_side_of_box;
+    //clamp
+     if (row >= 0 && row < game->how_many_boxes_in_side &&
+         col >= 0 && col < game->how_many_boxes_in_side) {
+         game->boxes[row][col].isAlive = true;
+     }    
+}
+
+struct Box** allocateBoxes(int how_many) {
+    struct Box** boxes = malloc(how_many * sizeof(struct Box*));
+    if (boxes == NULL) return NULL;
+    for (int i = 0; i < how_many; i++) {
+        boxes[i] = malloc(how_many * sizeof(struct Box));
+        if (boxes[i] == NULL) {
+            for (int j = 0; j < i; j++) free(boxes[j]);
+            free(boxes);
+            return NULL;
+        }
+    }
+    return boxes;
+}
+
 void freeing(struct Game *game,int exit_status)
 {
     SDL_DestroyRenderer(game->renderer);
     SDL_DestroyWindow(game->window);
-    for(int i=0;i<game->how_many_boxes_in_side;i++){
-            free(game->boxes[i]);
-    }
-    free(game->boxes);
+    freeBoxes(game->boxes,game->how_many_boxes_in_side);
+    freeBoxes(game->alt_boxes,game->how_many_boxes_in_side);
     SDL_Quit();
     exit(exit_status);
 }
